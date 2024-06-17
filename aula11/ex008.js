4d55dfbb8d40a6d6bbaca11243784cced5c7006c
AdaoPauloMiguel <104330874+AdaoPauloMiguel@users.noreply.github.com>
2024-06-18 02:20:33 +0300

    Estudando Funções em JS
    Fazendo meu primeiro exercício.

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 79
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if (vel > 60) { // Condição simples sem presença do 'else'.
+    console.log(`Você últrapassou o limite de velocidade permitida, será MULTADO!`)
+}
+console.log(`Use sempre o cinto de seguraça, dirija com cuidado!`)
+
