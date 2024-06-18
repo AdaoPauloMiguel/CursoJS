27cf5e6ea5c7de1b212ff6053306a8d40de94207
AdaoPauloMiguel <104330874+AdaoPauloMiguel@users.noreply.github.com>
2024-06-19 00:32:55 +0300

    Aplicando os conceitos de Repetições em JS
    Aprendi a usar as repetições while e do while.

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+// Para extruturas de repitições (DoWhile)
+
+var cont = 1 
+do {
+    console.log(`Tudo bem? Passo ${cont} com Do`) 
+    cont ++
+} while (cont <= 9);
+
+
+// Para Estruturas de repetição(While)
+
+var c = 1 
+while (c <= 6) {
+    console.log(`Tudo bem? Passo ${c} com while`)    
+    c ++
+}
