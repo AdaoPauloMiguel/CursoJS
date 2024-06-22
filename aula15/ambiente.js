e3392f9d7b0faae1e563846ce2e1f1dbd36bf19c
AdaoPauloMiguel <104330874+AdaoPauloMiguel@users.noreply.github.com>
2024-06-22 22:03:39 +0300

    Vector [array]
    Aprendendo sobre vetor e alguns incrementos básicos.

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3] 
+num[5] = 0
+num.push(4)
+num.sort()
+console.log(num)
+console.log(`Nosso vector tem ${num.length} posições`)
+console.log(`O segundo numero posicional do nosso array é ${num[1]}`)
